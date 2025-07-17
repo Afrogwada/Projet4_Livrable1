5d0a63ab3e6814213f8e8d00f68af73f3697213b
Afrogwada <afrogwadaweb@gmail.com>
2025-07-17 09:43:45 +0200

    Création data class 'note'

---
+++ app/src/main/java/com/openclassrooms/notes/model/Note.kt
@@ -0,0 +1,9 @@
+package com.openclassrooms.notes.model
+/**
+ * Represents a note.
+ * This class encapsulates all the details of a note, including the title and the body
+ */
+data class Note(
+    val title: String,
+    val body: String
+)
